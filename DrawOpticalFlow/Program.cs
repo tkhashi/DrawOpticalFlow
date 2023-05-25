0a6809dc258b2d159ba385ae2d2aa7ce1ffc44d7
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-05-25 18:37:42 +0900

    Refactor

--- DrawOpticalFlow/Program.cs
+++ DrawOpticalFlow/Program.cs
@@ -1,98 +1,63 @@
 ﻿using OpenCvSharp;
 
-var path = @"";
-var capture = new VideoCapture(path);
-var rng = new Random();
+var rnd = new Random();
+var colors = Enumerable
+    .Range(0, 100)
+    .Select(i => new Scalar(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)))
+    .ToArray();
 
 // Shi-Tomasiのコーナー検出パラメータ
-var maxCorners = 100;
-var qualityLevel = 0.3;
-var minDistance = 7;
-var blockSize = 7;
-
-// Lucas-Kanade法のパラメータ
-var winSize = new Size(15, 15);
-var maxLevel = 2;
-var criteria = new TermCriteria(CriteriaTypes.Eps | CriteriaTypes.Count, 10, 0.03);
-
-// ランダムに色を１００個生成（値0～255の範囲で100行3列のランダムなndarrayを生成）
-var colors = new Scalar[100];
-for (int i = 0; i < colors.Length; i++)
-{
-    colors[i] = new Scalar(rng.Next(0, 256), rng.Next(0, 256), rng.Next(0, 256));
-}
+const int maxCorners = 100;
+const double qualityLevel = 0.3;
+const int minDistance = 7;
+const int blockSize = 7;
+const bool useHarrisDetector = false;
+const double k = 0.04;
 
 // 最初のフレームの処理
-var frame = new Mat();
-bool endFlag = capture.Read(frame);
-var grayPrev = new Mat();
-bool useHarrisDetector = false;
-double k = 0.04;
-Cv2.CvtColor(frame, grayPrev, ColorConversionCodes.BGR2GRAY);
-var featuresPrev = Cv2.GoodFeaturesToTrack(grayPrev, maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector, k);
-var mask = new Mat(frame.Size(), MatType.CV_8UC3, Scalar.All(0));
+using var capture = new VideoCapture(@"C:\Users\k_tak\Downloads\534-21-62-2下流調査-ガタツキ部のみ.mp4");
+using var frameNext = new Mat();
+var endFlag = capture.Read(frameNext);
+var grayPrev = frameNext.CvtColor(ColorConversionCodes.BGR2GRAY);
+var featuresPrev =
+    grayPrev.GoodFeaturesToTrack(maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector, k);
+using var mask = new Mat(frameNext.Size(), MatType.CV_8UC3, Scalar.All(0));
 
 while (endFlag)
 {
-    // グレースケールに変換
-    var grayNext = new Mat();
-    Cv2.CvtColor(frame, grayNext, ColorConversionCodes.BGR2GRAY);
-
     // オプティカルフロー検出
-    Point2f[] featuresNext = new Point2f[] { };
-    byte[] status;
-    float[] err;
-    double minEigThreshold = 0.0001;
-    OpticalFlowFlags flags = OpticalFlowFlags.None;
-    Cv2.CalcOpticalFlowPyrLK(grayPrev, grayNext, featuresPrev, ref featuresNext, out status, out err, winSize, maxLevel, criteria, flags, minEigThreshold);
+    var featuresNext = new Point2f[] { };
+    var grayNext = frameNext.CvtColor(ColorConversionCodes.BGR2GRAY);
+    Cv2.CalcOpticalFlowPyrLK(
+        grayPrev,
+        grayNext,
+        featuresPrev,
+        ref featuresNext,
+        out var status,
+        out _,
+        new Size(15, 15),
+        2,
+        TermCriteria.Both(10, 0.03));
 
     // オプティカルフローを検出した特徴点を選別（0：検出せず、1：検出した）
-    var goodPrev = new List<Point2f>();
-    var goodNext = new List<Point2f>();
-    for (int i = 0; i < status.Length; i++)
-    {
+    for (var i = 0; i < status.Length; i++)
         if (status[i] == 1)
         {
-            goodPrev.Add(featuresPrev[i]);
-            goodNext.Add(featuresNext[i]);
+            // オプティカルフローを描画
+            mask.Line(featuresNext[i].ToPoint(), featuresPrev[i].ToPoint(), colors[i % 100], 2);
+            frameNext.Circle((int)featuresNext[i].X, (int)featuresNext[i].Y, 5, colors[i % 100], -1);
         }
-    }
 
-    // オプティカルフローを描画
-    for (int i = 0; i < goodNext.Count; i++)
-    {
-        var nextPoint = goodNext[i];
-        var prevPoint = goodPrev[i];
-        Cv2.Line(mask, nextPoint.ToPoint(), prevPoint.ToPoint(), colors[i % 100], 2);
-        Cv2.Circle(frame, (int)nextPoint.X, (int)nextPoint.Y, 5, colors[i % 100], -1);
-    }
-
-    var img = new Mat();
-    Cv2.Add(frame, mask, img);
-
-    // ウィンドウに表示
-    Cv2.ImShow("window", img);
-
-    // ESCキー押下で終了
-    if ((Cv2.WaitKey(30) & 0xff) == 27)
-        break;
+    using var drawnMat = frameNext.Add(mask);
+    Cv2.ImShow("flow", drawnMat);
+    Cv2.WaitKey(1);
 
     // 次のフレーム、ポイントの準備
     grayPrev = grayNext.Clone();
-    if (goodNext.Count > 0)
-    {
-        featuresPrev = goodNext.ToArray();
-    }
-    else
-    {
-        // 特徴点が見つからなかった場合、再度特徴点を検出
-        featuresPrev = Cv2.GoodFeaturesToTrack(grayPrev, maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector, k);
-    }
+    featuresPrev = status.Contains((byte)1)
+        ? featuresNext
+        : Cv2.GoodFeaturesToTrack(grayPrev, maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector,
+            k); // 特徴点が見つからなかった場合、再度特徴点を検出
 
-    endFlag = capture.Read(frame);
+    endFlag = capture.Read(frameNext);
 }
-
-// 終了処理
-Cv2.DestroyAllWindows();
-capture.Release();
-

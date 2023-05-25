220e7797d4c4ae860a0eda25dd5973524901708b
k_takahashi <k_takahashi@just-ltd.co.jp>
2023-05-25 20:52:01 +0900

    Add comment

--- DrawOpticalFlow/Program.cs
+++ DrawOpticalFlow/Program.cs
@@ -6,12 +6,20 @@ var colors = Enumerable
     .Select(i => new Scalar(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)))
     .ToArray();
 
-// Shi-Tomasiのコーナー検出パラメータ
+// 検出する特徴点の最高数
 const int maxCorners = 100;
+// 特徴点のスコアの閾値。0~1で1が最高。
 const double qualityLevel = 0.3;
-const int minDistance = 7;
+// 特徴点間の最小距離
+const int minDistance = 100;
+//  特徴点の検出範囲を指定するマスク行列。nullだと画像全体で検出。
+Mat rangeMask = null;
+// 特徴点の検出に使用される近傍領域のサイズ
 const int blockSize = 7;
-const bool useHarrisDetector = false;
+// falseではShi-Tomasi検出器になる
+const bool useHarrisDetector = true;
+// ハリスのコーナー検出器で使う自由パラメータ。通常0.04~0.06を指定する。
+// 小さいとコーナーが多く検出される。大きいとより強い勾配の変化をもつ点が検出される。
 const double k = 0.04;
 
 // 最初のフレームの処理
@@ -21,7 +29,7 @@ using var frameNext = new Mat();
 var endFlag = capture.Read(frameNext);
 var grayPrev = frameNext.CvtColor(ColorConversionCodes.BGR2GRAY);
 var featuresPrev =
-    grayPrev.GoodFeaturesToTrack(maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector, k);
+    grayPrev.GoodFeaturesToTrack(maxCorners, qualityLevel, minDistance, rangeMask, blockSize, useHarrisDetector, k);
 using var mask = new Mat(frameNext.Size(), MatType.CV_8UC3, Scalar.All(0));
 
 while (endFlag)
@@ -34,9 +42,9 @@ while (endFlag)
         grayNext,
         featuresPrev,
         ref featuresNext,
-        out var status,
-        out _,
-        new Size(15, 15),
+        out var status, 
+        err:out _,
+        winSize:new Size(21, 21),
         2,
         TermCriteria.Both(10, 0.03));
 
@@ -51,14 +59,19 @@ while (endFlag)
 
     using var drawnMat = frameNext.Add(mask);
     Cv2.ImShow("flow", drawnMat);
-    Cv2.WaitKey(1);
+    Cv2.WaitKey();
 
     // 次のフレーム、ポイントの準備
     grayPrev = grayNext.Clone();
-    featuresPrev = status.Contains((byte)1)
-        ? featuresNext
-        : Cv2.GoodFeaturesToTrack(grayPrev, maxCorners, qualityLevel, minDistance, null, blockSize, useHarrisDetector,
-            k); // 特徴点が見つからなかった場合、再度特徴点を検出
+    if (status.Contains((byte)1))
+    {
+        featuresPrev = featuresNext;
+    }
+    else
+    { 
+        // 特徴点が見つからなかった場合、再度特徴点を検出
+        featuresPrev = grayPrev.GoodFeaturesToTrack(maxCorners, qualityLevel, minDistance, rangeMask, blockSize, useHarrisDetector, k);
+    }
 
     endFlag = capture.Read(frameNext);
 }
